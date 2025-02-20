cddd7d3e75f1b34817cd8e1f05c5c456e6b8c587
Catelynn <45514373+catelynncai@users.noreply.github.com>
2025-02-20 13:07:32 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
+// holidays
+  const holidays = ["valentines", "halloween", "christmas", "new years", "birthday"]
 
+  //this doesnt work
+  for (let i = 0; i < holidays.length; i++) {
+              let btn = document.createElement("button");
+              btn.innerText = holidays[i].text;
+  }
+  
+  // valentines
+  const angelfish = ["", "french angelfish mate for life"]
+  const seahorse = ["", "seahorses mate for life"]
+  const koi = ["you've got me feeling koi around you", "in asian countries, koi are a symbol of love and prosperity"]
+  const valentines = [angelfish, seahorse, koi]
+
+  // halloween
+  const piranhas = ["", "piranhas are scary"]
+  const anglerfish = ["", "anglerfish live in the deep sea"]
+  const goblinshark = ["Gonna be goblin that candy up!", "scary"]
+  const halloween = [piranhas, anglerfish, goblinshark]
