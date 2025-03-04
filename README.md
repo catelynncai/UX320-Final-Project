2f53cf19ee3590b165495314905083cf5ef81dca
Catelynn <catelynn.kk.cai@gmail.com>
2025-03-04 18:41:55 -0500

    WIP of JS
    Added navigation between pages, still figuring out storing the dropdown fucntion

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Catelynn and Avery
 Summary description 
 
 ## Instructions 
-
+Run index.html and input the desired names, message, and occasion for your fish themed card. Then, press submit.
 
 ## Used Libraries & Resources
 Bootstrap & Popper
--- javascript.js
+++ javascript.js
@@ -1,11 +1,8 @@
-// holidays
-  const holidays = ["birthday","valentines", "halloween", "christmas", "new years"]
-  
-  // valentines
-  const angelfish = ["", "french angelfish mate for life"]
-  const seahorse = ["", "seahorses mate for life"]
-  const koi = ["you've got me feeling koi around you", "in asian countries, koi are a symbol of love and prosperity"]
-  const valentines = [angelfish, seahorse, koi]
+ // valentines
+ const angelfish = ["", "french angelfish mate for life"]
+ const seahorse = ["", "seahorses mate for life"]
+ const koi = ["you've got me feeling koi around you", "in asian countries, koi are a symbol of love and prosperity"]
+ const valentines = [angelfish, seahorse, koi]
 
   // halloween
   const piranhas = ["", "piranhas are scary"]
@@ -13,35 +10,36 @@
   const goblinshark = ["Gonna be goblin that candy up!", "scary"]
   const halloween = [piranhas, anglerfish, goblinshark]
 
-  /*let submitButton = document.createElement("button");
-            submitButton.innerText = "Generate";
-
-            submitButton.onclick = function() { 
-                //if()
-            };*/
-
-// inputs
-  /*function sayHello() {
-  const name = document.getElementById('username').value;
-  document.getElementById('output').innerText = 'Hi, ' + name + ', Welcome to my website!';
-  }*/
-
-  // Makes it so that the selected option appears in the dropdown box
-  function selectMenu(selectedItem) {
-    document.getElementById("dropdownButton").innerText = selectedItem;
-  }
-
-  // this doesn't work
-  function storeSelection() {
-    localStorage.setItem('holidaychoice', document.getElementById("dropdownButton").innerText)
-  }
-  
   // stores each input to variables to reuse in cards
   function storeInput() {
     localStorage.setItem('userNameInput', document.getElementById('userNameInput').value);  // Store the input in local storage
     localStorage.setItem('recNameInput', document.getElementById('recNameInput').value); // Store input in local storage
     localStorage.setItem('messageInput', document.getElementById('messageInput').value); // stores message in local storage
-    localStorage.setItem('holiday', document.getElementById('holidayInput').innerHTML); // stores message in local storage
-    //localStorage.setItem('holidaychoice', document.getElementById('holiday').value);
+    //localStorage.setItem('holiday', document.getElementById('holidayInput').value); // stores holiday in local storage
+    
     //window.location.href = 'fish2.html';  // Redirect to result page
+
+    //selectRandom()
   }
+
+
+  //figuring out array stuff
+  function selectRandom(){
+    const holidaychoice = getItem(holiday)
+    console.log(holidaychoice)
+
+    
+    if (holidaychoice = "Valentine's"){
+      const holidayfish = valentines[Math.floor(Math.random() * valentines.length)];
+      console.log(holidayfish)
+    }else{
+      console.log("i fucked up")
+    }
+    
+  }
+
+
+
+function regenerate(){
+  // select other randoms based on storage
+}
