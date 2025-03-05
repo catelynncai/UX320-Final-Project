e2dec949238812a1e17299c686a0cc47156a3132
Catelynn <catelynn.kk.cai@gmail.com>
2025-03-04 23:06:17 -0500

    Organized Storage
    randomized puns/facts now appear on the correct page, added other holidays to array database (still work in progress)

--- javascript.js
+++ javascript.js
@@ -3,8 +3,7 @@
     localStorage.setItem('userNameInput', document.getElementById('userNameInput').value);  // Store the input in local storage
     localStorage.setItem('recNameInput', document.getElementById('recNameInput').value); // Store input in local storage
     localStorage.setItem('messageInput', document.getElementById('messageInput').value); // stores message in local storage
-    localStorage.setItem('holiday', document.getElementById('holidayInput').innerHTML); // stores holiday in local storage
-
+    
     // Store selected value from the dropdown in local storage
     const holidaySelect = document.getElementById('holidayInput');
     const selectedHoliday = holidaySelect.options[holidaySelect.selectedIndex].value;
@@ -26,8 +25,8 @@
     // SELECTED VALENTINES
     if (document.getElementById('holidayInput').value=="valentines"){
         //fish data
-          const angelfish = ["angelfish", "fishpun", "french angelfish mate for life"]
-          const seahorse = ["seahorse", "fishpun", "seahorses mate for life"]
+          const angelfish = ["angelfish", "angelfishpun", "french angelfish mate for life"]
+          const seahorse = ["seahorse", "seahorsefishpun", "seahorses mate for life"]
           const koi = ["koi", "you've got me feeling koi around you", "in asian countries, koi are a symbol of love and prosperity"]
         //possible fish
           const valentines = [angelfish, seahorse, koi]
@@ -36,15 +35,37 @@
         const number = Math.floor(Math.random() * valentines.length);
         console.log(number);
         let fish = valentines[number][0];
+        localStorage.setItem('fishvar', fish); // stores fish in local storage
         let fishpun = valentines[number][1];
+        localStorage.setItem('punvar', fishpun); // stores pun in local storage
         let fishfact = valentines[number][2];
-        console.log("fish: " + fish + "pun:" + fishpun + "fact: " + fishfact)
+        localStorage.setItem('factvar', fishfact); // stores fact in local storage
     }
+
+    // SELECTED BIRTHDAYS (WIP)
+    if (document.getElementById('holidayInput').value=="birthday"){
+      //fish data
+        const angelfish = ["angelfish", "angelfishpun", "french angelfish mate for life"]
+        const seahorse = ["seahorse", "seahorsefishpun", "seahorses mate for life"]
+        const koi = ["koi", "you've got me feeling koi around you", "in asian countries, koi are a symbol of love and prosperity"]
+      //possible fish
+        const birthday = [angelfish, seahorse, koi]
+
+      // random fish selection
+      const number = Math.floor(Math.random() * birthday.length);
+      console.log(number);
+      let fish = birthday[number][0];
+      localStorage.setItem('fishvar', fish); // stores fish in local storage
+      let fishpun = birthday[number][1];
+      localStorage.setItem('punvar', fishpun); // stores pun in local storage
+      let fishfact = birthday[number][2];
+      localStorage.setItem('factvar', fishfact); // stores fact in local storage
+  }
 
     // SELECTED HALLOWEEN
     if (document.getElementById('holidayInput').value=="halloween"){
-      const piranhas = ["piranha", "fishpun", "piranhas are scary"]
-      const anglerfish = ["anglerfish", "fishpun", "anglerfish live in the deep sea"]
+      const piranhas = ["piranha", "piranha fishpun", "piranhas are scary"]
+      const anglerfish = ["anglerfish", "anglerfish fishpun", "anglerfish live in the deep sea"]
       const goblinshark = ["goblinshark", "Gonna be goblin that candy up!", "scary"]
       const halloween = [piranhas, anglerfish, goblinshark]
 
@@ -52,11 +73,30 @@
       const number = Math.floor(Math.random() * halloween.length);
       console.log(number);
       let fish = halloween[number][0];
+      localStorage.setItem('fishvar', fish); // stores fish in local storage
       let fishpun = halloween[number][1];
+      localStorage.setItem('punvar', fishpun); // stores pun in local storage
       let fishfact = halloween[number][2];
-      console.log("fish: " + fish + "pun:" + fishpun + "fact: " + fishfact)
+      localStorage.setItem('factvar', fishfact); // stores fact in local storage
   }
     
+// SELECTED NEW YEARS (WIP)
+if (document.getElementById('holidayInput').value=="newyears"){
+  //fish data
+    const angelfish = ["angelfish", "It's time to reel in the New Year!", "french angelfish mate for life"]
+  //possible fish
+    const newyears = [angelfish, seahorse, koi]
+
+  // random fish selection
+  const number = Math.floor(Math.random() * newyears.length);
+  console.log(number);
+  let fish = newyears[number][0];
+  localStorage.setItem('fishvar', fish); // stores fish in local storage
+  let fishpun = newyears[number][1];
+  localStorage.setItem('punvar', fishpun); // stores pun in local storage
+  let fishfact = newyears[number][2];
+  localStorage.setItem('factvar', fishfact); // stores fact in local storage
+}
   }
 
 
